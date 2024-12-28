9bfe666fa1a793832774a2f7c72bbae1ff7f0a0c
igyfhc <23456@asf.com>
2024-12-28 22:10:13 +0800

    path

--- .apps/blog/docs/.vitepress/config.mjs
+++ .apps/blog/docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    outDir: 'docs/.vitepress/dist/blog',
+    outDir: '.vitepress/dist/blog',
     base: '/blog/',
     title: 'My Awesome Project',
     description: 'A VitePress Site',
