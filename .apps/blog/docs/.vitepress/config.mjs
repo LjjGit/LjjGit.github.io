b6f0057823f7a07fb9440e968998ba6247d44cbe
igyfhc <23456@asf.com>
2024-12-28 23:33:48 +0800

    github-pages

--- .apps/blog/docs/.vitepress/config.mjs
+++ .apps/blog/docs/.vitepress/config.mjs
@@ -14,8 +14,8 @@ export default defineConfig({
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
         nav: [
-            {text: 'Home', link: '/'},
-            {text: 'Examples', link: '/markdown-examples'},
+            {text: '首页', link: '/'},
+            {text: '示例', link: '/markdown-examples'},
         ],
 
         sidebar: [
