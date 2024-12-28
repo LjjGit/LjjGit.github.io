d724703b508e698eadb4a27bdeed11e806c90dab
igyfhc <23456@asf.com>
2024-12-28 22:39:03 +0800

    构建脚本

--- .apps/blog/docs/.vitepress/config.mjs
+++ .apps/blog/docs/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    outDir: '.vitepress/dist/blog',
+    outDir: '../../../.deploy/blog',
     base: '/blog/',
     title: 'My Awesome Project',
     description: 'A VitePress Site',
