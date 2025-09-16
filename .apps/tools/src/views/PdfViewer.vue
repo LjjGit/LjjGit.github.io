4a7554bef49175c2d76315da2c05d2d2f5222b93
redmi <redmi@example.com>
2025-09-16 22:48:22 +0800

    iframe

--- .apps/tools/src/views/PdfViewer.vue
+++ .apps/tools/src/views/PdfViewer.vue
@@ -1,10 +1,29 @@
 <template>
-    <input type="file" @change="e => handleViewClick(e.target.files[0])">
+    <div class="box">
+        <input class="form-control" type="file" @change="e => handleViewClick(e.target.files[0])">
+        <iframe class="frame" :src></iframe>
+    </div>
 </template>
 
 <script setup>
+const src = ref();
 function handleViewClick(file)
 {
-    location.href = `/pdfviewer/web/viewer.html?file=${URL.createObjectURL(file)}`;
+    src.value = `/pdfviewer/web/viewer.html?file=${URL.createObjectURL(file)}`;
 }
 </script>
+
+<style scoped>
+.box {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+}
+.frame {
+    flex: auto;
+}
+</style>
